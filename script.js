2cb6fdd06473ab594b87fa9f1c8d312e65c4a582
¨hbonavota¨ <hbonavota@gmail.com>
2020-11-22 20:48:10 +0100

    add some styles of css

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ $.ajax({
     console.log(data.weather[0].description)
     $("#ciudad").html(data.name)
     $("#temperatura").html(data.main.temp)
+    $("#temperaturaMin").html(data.main.temp_min)
+    $("#temperaturaMax").html(data.main.temp_max)
     $("#estado_del_cielo").html(data.weather[0].description)
 
   }
@@ -28,6 +30,9 @@ $.ajax({
     console.log(data.weather[0].description)
     $("#ciudad").html(data.name)
     $("#temperatura").html(data.main.temp)
+    $("#temperaturaMin").html(data.main.temp_min)
+    $("#temperaturaMax").html(data.main.temp_max)
+    $("#humidity").html(data.main.humidity)
     $("#estado_del_cielo").html(data.weather[0].description)
 
   }
@@ -43,6 +48,9 @@ $.ajax({
     console.log(data.weather[0].description)
     $("#ciudad").html(data.name)
     $("#temperatura").html(data.main.temp)
+    $("#temperaturaMin").html(data.main.temp_min)
+    $("#temperaturaMax").html(data.main.temp_max)
+    $("#humidity").html(data.main.humidity)
     $("#estado_del_cielo").html(data.weather[0].description)
 
   }
@@ -59,6 +67,9 @@ $.ajax({
     console.log(data.weather[0].description)
     $("#ciudad").html(data.name)
     $("#temperatura").html(data.main.temp)
+    $("#temperaturaMin").html(data.main.temp_min)
+    $("#temperaturaMax").html(data.main.temp_max)
+    $("#humidity").html(data.main.humidity)
     $("#estado_del_cielo").html(data.weather[0].description)
 
   }
@@ -68,18 +79,20 @@ $.ajax({
 
 
 $("#boton").click( 
-  function() 
-      {
-        var ciudad = $("input").val()
-        $.ajax({
-          method: 'GET',
-          url: 'http://api.openweathermap.org/data/2.5/weather?q=' +ciudad + '&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es',
-          success: function(data){
-              console.log(data.weather[0].description)
-              $("#ciudad").html(data.name)
-              $("#temperatura").html(data.main.temp)
-              $("#estado_del_cielo").html(data.weather[0].description)
-              }
-      })
+  function(){
+  var ciudad = $("input").val()
+  $.ajax({
+    method: 'GET',
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=' +ciudad + '&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es',
+    success: function(data){
+      console.log(data.weather[0].description)
+      $("#ciudad").html(data.name)
+      $("#temperatura").html(data.main.temp)
+      $("#temperaturaMin").html(data.main.temp_min)
+      $("#temperaturaMax").html(data.main.temp_max)
+      $("#humidity").html(data.main.humidity)
+      $("#estado_del_cielo").html(data.weather[0].description)
+    }
+  })
 })
 
